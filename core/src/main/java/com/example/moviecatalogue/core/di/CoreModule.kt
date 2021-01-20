50a4e38c915152bd977e454fe519f0aa4f605f7c
royasgaswa <royasgaswa99@gmail.com>
2021-01-20 10:57:01 +0700

    '

--- core/src/main/java/com/example/moviecatalogue/core/di/CoreModule.kt
+++ core/src/main/java/com/example/moviecatalogue/core/di/CoreModule.kt
@@ -8,6 +8,9 @@ import com.example.moviecatalogue.core.data.source.remote.RemoteDataSource
 import com.example.moviecatalogue.core.data.source.remote.network.ApiService
 import com.example.moviecatalogue.core.domain.repository.ICatalogueRepository
 import com.example.moviecatalogue.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -24,21 +27,34 @@ val databaseModule= module {
         get<CatalogueDatabase>().tvshowDao()
     }
     single {
+        val passhrase:ByteArray=SQLiteDatabase.getBytes("catalogue".toCharArray())
+        val factory = SupportFactory(passhrase)
         Room.databaseBuilder(
             androidContext(),
             CatalogueDatabase::class.java,"Catalogue.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 val networkModule = module {
     single {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname,"sha256/+vqZVAzTqUP8BGkfl88yU7SQ3C8J2uNEa55B7RZjEg0=")
+            .add(hostname,"sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA=")
+            .add(hostname,"sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+            .add(hostname,"sha256/KwccWaCgrnaw6tsrrSO61FgLacNgG2MMLq8GE6+oP5I=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
+
         val retrofit = Retrofit.Builder()
             .baseUrl("https://api.themoviedb.org/3/")
             .addConverterFactory(GsonConverterFactory.create())
